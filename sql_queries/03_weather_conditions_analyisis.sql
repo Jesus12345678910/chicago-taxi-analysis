19f0041ab2533b78ac5255dd4cbe6df45e61998f
Jesus12345678910 <36488583+Jesus12345678910@users.noreply.github.com>
2026-02-12 14:23:46 -0600

    Update 03_weather_conditions_analyisis.sql

--- sql_queries/03_weather_conditions_analyisis.sql
+++ sql_queries/03_weather_conditions_analyisis.sql
@@ -1,17 +1,8 @@
-SELECT 
-    start_ts,
-        CASE 
-        WHEN (description LIKE '%rain%') OR 
-        (description LIKE '%storm%') THEN 'Bad'
+SELECT
+    ts,
+    CASE
+        WHEN description LIKE '%rain%' OR description LIKE '%storm%' THEN 'Bad'
         ELSE 'Good'
-    END AS weather_conditions,
-    duration_seconds
+    END AS weather_conditions
 FROM 
-    trips
-    INNER JOIN weather_records ON trips.start_ts = weather_records.ts
-WHERE
-    pickup_location_id = 50 AND dropoff_location_id = 63 
-    AND 
-    EXTRACT (DOW from trips.start_ts) = 6
-ORDER BY
-    trip_id;
+    weather_records;
